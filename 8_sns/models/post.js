8f68c74d8872486d9b6321f6155e7552e23106f8
taeyun <lwe12f675@gmail.com>
2025-01-31 23:52:32 +0900

    feat: 데이터베이스 셋팅 - Post테이블 생성

--- 8_sns/models/post.js
+++ 8_sns/models/post.js
@@ -1,7 +1,31 @@
 const Sequelize = require("sequelize");
 
 class Post extends Sequelize.Model {
-  static initiate(sequelize) {}
+  static initiate(sequelize) {
+    Post.init(
+      {
+        content: {
+          type: Sequelize.STRING(140),
+          allowNull: false,
+        },
+        // 우선 이미지 한개만 올리는 것으로 함
+        img: {
+          type: Sequelize.STRING(200),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Post",
+        tableName: "posts",
+        paranoid: false, // 게시글은 복구가 필요없으므로 false, 필요하다면 true하면 알아서 복구하게 됨
+        charset: "utf8mb4", // 게시글은 이모티콘 허용
+        collate: "utf8mb4_general_ci",
+      }
+    );
+  }
 
   static associate(db) {}
 }
