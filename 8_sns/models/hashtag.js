c9d33f2895b6a5eb451540aa151c9599157465b0
taeyun <lwe12f675@gmail.com>
2025-01-31 23:54:50 +0900

    feat: 데이터베이스 셋팅 - Hashtag테이블 생성

--- 8_sns/models/hashtag.js
+++ 8_sns/models/hashtag.js
@@ -1,7 +1,28 @@
 const Sequelize = require("sequelize");
 
 class Hashtag extends Sequelize.Model {
-  static initiate(sequelize) {}
+  static initiate(sequelize) {
+    // 해시태그는 이름만 있으면 됨
+    Hashtag.init(
+      {
+        title: {
+          type: Sequelize.STRING(15),
+          allowNull: false, // 필수
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Hashtag",
+        tableName: "hashtags",
+        paranoid: false,
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
+  }
 
   static associate(db) {}
 }
