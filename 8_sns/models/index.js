c055f3b16d1376c847f64c318d7e7c2b5ad3517b
taeyun <lwe12f675@gmail.com>
2025-02-01 00:14:19 +0900

    feat: 데이터베이스 셋팅 - models/index.js 파일 수정

--- 8_sns/models/index.js
+++ 8_sns/models/index.js
@@ -1,43 +1,43 @@
-'use strict';
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+db.sequelize = sequelize;
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+const basename = path.basename(__filename); // 현재 파일 이름 index.js
+
+fs.readdirSync(__dirname) // 현재 폴더(models)의 모든 파일을 조회
+  .filter((file) => {
+    // 모델이 아닌 파일이 들어가지 안게끔 필터링
+    // 숨김 파일(.env같은), index.js, js 확장자가 아닌 파일 필터링 (index.js는 모델이 아니므로 제외)
     return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    // 해당 파일의 모델 불러와서 init
+    const model = require(path.join(__dirname, file)); // models 폴더 내의 파일들을 불러옴, 필터된 애들 빼고
+    console.log(file, model.name);
     db[model.name] = model;
+    model.initiate(sequelize); // 모델 파일들을 초기화
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
+  console.log(db, modelName);
+  // associate 호출
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
