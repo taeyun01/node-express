35f39c170bae16764501daadab8ba03247db7f7e
taeyun <lwe12f675@gmail.com>
2025-02-05 02:09:08 +0900

    feat: passport를 사용해 카카오로그인 구현

--- 8_sns/passport/kakaoStrategy.js
+++ 8_sns/passport/kakaoStrategy.js
@@ -1,3 +1,41 @@
+const passport = require("passport");
+const { Strategy: KakaoStrategy } = require("passport-kakao");
+const User = require("../models/user");
+
 module.exports = () => {
-  // 카카오 로그인 체크
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID, // 카카오 앱 아이디
+        callbackURL: "/auth/kakao/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log("profile", profile); // 카카오 사용자 정보가 들어있음 (카카오에서 정보를 계속 바뀌므로 콘솔로 확인)
+
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "kakao" },
+          });
+
+          // 이미 가입된 사용자라면 로그인
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            // 가입되지 않은 사용자라면 회원가입
+            const newUser = await User.create({
+              email: profile._json && profile._json.kakao_account.email, // 이 구조가 바뀔수도 있기 때문에 위 profile 콘솔 확인
+              nick: profile.displayName,
+              snsId: profile.id,
+              provider: "kakao",
+            });
+
+            done(null, newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          done(error);
+        }
+      }
+    )
+  );
 };
--- 8_sns/routes/auth.js
+++ 8_sns/routes/auth.js
@@ -15,4 +15,19 @@ router.post("/login", isNotLoggedIn, login);
 // POST /auth/logout (로그아웃, 로그인 한 사람들만 가능)
 router.get("/logout", isLoggedIn, logout);
 
+// /auth/kakao
+router.get("/kakao", passport.authenticate("kakao")); // 카카오 로그인 화면으로 redirect
+
+// 카카오 로그인 화면에서 로그인이 완료되면 callback으로 이동
+// /auth/kakao/callback
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/?loginError=카카오로그인 실패", // 로그인 실패시 리다이렉트
+  }),
+  (req, res) => {
+    res.redirect("/"); // 성공시 메인
+  }
+);
+
 module.exports = router;
